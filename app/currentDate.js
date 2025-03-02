6b43df927a22c086424768094f2ab9bd0fa96bca
FarhanAfrad1 <farhan.tanmoy13@gmail.com>
2025-03-02 22:49:51 +0600

    blog question is answered

--- app/currentDate.js
+++ app/currentDate.js
@@ -2,13 +2,13 @@
 const newDate = new Date();
 
 const formattedDate = new Intl.DateTimeFormat("en-US", {
-  weekday: "short", // "Wed"
-  month: "short",   // "Jul"
-  day: "2-digit",   // "28"
-  year: "numeric"   // "2025"
-}); // Remove extra comma
+  weekday: "short",
+  month: "short", 
+  day: "2-digit", 
+  year: "numeric" 
+}); 
 const parts = formattedDate.formatToParts(newDate);
-console.log(parts);
+
 const datePart1 = `${parts[0].value},`;
 const datePart2 = `${parts[2].value} ${parts[4].value} ${parts[6].value}`
 updateInnerText('day',datePart1);
